6b8b55624401bed3bc784e40946655af7ab2cc80
Daniel Pérez Pedrosa <71631573+danipp01@users.noreply.github.com>
2020-11-30 13:59:23 +0100

    Update Editor_Texto.java

--- src/ejerciciosProgramacion/EjercicioEditorTexto/Editor_Texto.java
+++ src/ejerciciosProgramacion/EjercicioEditorTexto/Editor_Texto.java
@@ -6,7 +6,7 @@ import javax.swing.JOptionPane;
 
 import ejerciciosProgramacion.Utils;
 
-public class Ejercicio1 {
+public class Editor_Textos {
 
 	public static void main(String[] args) {
 		// Declaro variables necesarias
